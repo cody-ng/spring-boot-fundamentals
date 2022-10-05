70a90294cec18cf9100f1f0a35dffeb91f0acc80
Cody Ng <cng@vitralogy.com>
2022-10-05 00:28:49 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/fundamentals/service/ApplicationService.java
@@ -0,0 +1,13 @@
+package com.pluralsight.fundamentals.service;
+
+import com.pluralsight.fundamentals.entity.Application;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ApplicationService {
+    List<Application> listApplications();
+    Application findApplication(long id);
+}
+
+
